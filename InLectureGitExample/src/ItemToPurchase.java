61041aab9340e9a7da0aba104172470ce78bca8a
christian <christian@christians-mbp.hsd1.pa.comcast.net>
2020-10-15 13:17:40 -0400

    Created getters and setters

--- InLectureGitExample/src/ItemToPurchase.java
+++ InLectureGitExample/src/ItemToPurchase.java
@@ -1,11 +1,28 @@
 
 public class ItemToPurchase {
 
-    //Private fields
-    String itemName; //- Initialized in default constructor to "none"
-    int itemPrice; //- Initialized in default constructor to 0
-    int itemQuantity; //- Initialized in default constructor to 0
+    String itemName; 
+    int itemPrice; 
+    int itemQuantity;
+    public String getItemName() {
+        return itemName;
+    }
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+    public int getItemPrice() {
+        return itemPrice;
+    }
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+    public int getItemQuantity() {
+        return itemQuantity;
+    }
+    public void setItemQuantity(int itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    } 
     
-    //Default Constructor
+    
     
 }
