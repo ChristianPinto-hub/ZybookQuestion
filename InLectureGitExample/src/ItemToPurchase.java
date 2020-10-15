83cf82732e66414667d6927368a21beb94a00e8b
christian <christian@christians-mbp.hsd1.pa.comcast.net>
2020-10-15 12:42:46 -0400

    Created instance variables

--- InLectureGitExample/src/ItemToPurchase.java
+++ InLectureGitExample/src/ItemToPurchase.java
@@ -1,4 +1,11 @@
 
 public class ItemToPurchase {
 
+    //Private fields
+    String itemName; //- Initialized in default constructor to "none"
+    int itemPrice; //- Initialized in default constructor to 0
+    int itemQuantity; //- Initialized in default constructor to 0
+    
+    //Default Constructor
+    
 }
